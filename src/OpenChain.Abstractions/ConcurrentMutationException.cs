ff66f54b849e59587b543c262dd1c3dc9534330e
Flavien Charlon <flavien@charlon.net>
2015-07-26 11:11:50 +0100

    Use string interpolation in the ConcurrentMutationException class

--- src/OpenChain.Abstractions/ConcurrentMutationException.cs
+++ src/OpenChain.Abstractions/ConcurrentMutationException.cs
@@ -5,10 +5,7 @@ namespace OpenChain
     public class ConcurrentMutationException : Exception
     {
         public ConcurrentMutationException(Record failedMutation)
-            : base(string.Format(
-                "Version '{0}' of key '{1}' no longer exists.",
-                failedMutation.Version.ToString(),
-                failedMutation.Key.ToString()))
+            : base($"Version '{failedMutation.Version}' of key '{failedMutation.Key}' no longer exists.")
         {
             this.FailedMutation = failedMutation;
         }
