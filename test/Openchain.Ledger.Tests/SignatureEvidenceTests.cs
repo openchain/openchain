bb8d031b9ccf3355f3816afbabf57a350914a127
Flavien Charlon <flavien@charlon.net>
2015-11-19 11:50:12 +0000

    Improve test coverage for the ECKey class

--- test/Openchain.Ledger.Tests/SignatureEvidenceTests.cs
+++ test/Openchain.Ledger.Tests/SignatureEvidenceTests.cs
@@ -19,12 +19,13 @@ namespace Openchain.Ledger.Tests
     public class SignatureEvidenceTests
     {
         private readonly ByteString payload = ByteString.Parse("b27982ff2a4fa0c163da8651ca1902e27b651e68a6d0a4f6bff4a78d7dcf718d");
+        private readonly ByteString publicKey = ByteString.Parse("0213b0006543d4ab6e79f49559fbfb18e9d73596d63f39e2f12ebc2c9d51e2eb06");
 
         [Fact]
         public void VerifySignature_Valid()
         {
             SignatureEvidence signature = new SignatureEvidence(
-                ByteString.Parse("0213b0006543d4ab6e79f49559fbfb18e9d73596d63f39e2f12ebc2c9d51e2eb06"),
+                publicKey,
                 ByteString.Parse("304402200c7fba6b623efd7e52731a11e6d7b99c2ae752c0f950b7a444ef7fb80162498c02202b01c74a4a04fb120860494de09bd6848f088927a7b07e3c3925b3894c8c89d4"));
 
             Assert.True(signature.VerifySignature(payload.ToByteArray()));
@@ -44,8 +45,20 @@ namespace Openchain.Ledger.Tests
         public void VerifySignature_InvalidSignature()
         {
             SignatureEvidence signature = new SignatureEvidence(
-                ByteString.Parse("0213b0006543d4ab6e79f49559fbfb18e9d73596d63f39e2f12ebc2c9d51e2eb06"),
+                publicKey,
                 ByteString.Parse("304402200c7fba6b623efd7e52731a11e6d7b99c2ae752c0f950b7a444ef7fb80162498c02202b01c74a4a04fb120860494de09bd6848f088927a7b07e3c3925b3894c8c89d5"));
+
+            Assert.False(signature.VerifySignature(payload.ToByteArray()));
+
+            signature = new SignatureEvidence(
+                publicKey,
+                ByteString.Parse("3044"));
+
+            Assert.False(signature.VerifySignature(payload.ToByteArray()));
+
+            signature = new SignatureEvidence(
+                publicKey,
+                ByteString.Parse("300680044164616D"));
 
             Assert.False(signature.VerifySignature(payload.ToByteArray()));
         }
