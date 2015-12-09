c7457b4f041e2ba97e84022852b9f9cd3b8e32d9
Flavien Charlon <flavien@charlon.net>
2015-12-09 16:04:16 +0000

    Improve test coverage for the SignatureEvidence class

--- test/Openchain.Ledger.Tests/SignatureEvidenceTests.cs
+++ test/Openchain.Ledger.Tests/SignatureEvidenceTests.cs
@@ -20,55 +20,60 @@ namespace Openchain.Ledger.Tests
     {
         private readonly ByteString payload = ByteString.Parse("b27982ff2a4fa0c163da8651ca1902e27b651e68a6d0a4f6bff4a78d7dcf718d");
         private readonly ByteString publicKey = ByteString.Parse("0213b0006543d4ab6e79f49559fbfb18e9d73596d63f39e2f12ebc2c9d51e2eb06");
+        private readonly ByteString signature = ByteString.Parse("304402200c7fba6b623efd7e52731a11e6d7b99c2ae752c0f950b7a444ef7fb80162498c02202b01c74a4a04fb120860494de09bd6848f088927a7b07e3c3925b3894c8c89d4");
 
         [Fact]
         public void VerifySignature_Valid()
         {
-            SignatureEvidence signature = new SignatureEvidence(
-                publicKey,
-                ByteString.Parse("304402200c7fba6b623efd7e52731a11e6d7b99c2ae752c0f950b7a444ef7fb80162498c02202b01c74a4a04fb120860494de09bd6848f088927a7b07e3c3925b3894c8c89d4"));
+            SignatureEvidence evidence = new SignatureEvidence(publicKey, signature);
 
-            Assert.True(signature.VerifySignature(payload.ToByteArray()));
+            Assert.True(evidence.VerifySignature(payload.ToByteArray()));
         }
 
         [Fact]
         public void VerifySignature_InvalidPublicKey()
         {
-            SignatureEvidence signature = new SignatureEvidence(
+            SignatureEvidence evidence = new SignatureEvidence(
                 ByteString.Parse("0013b0006543d4ab6e79f49559fbfb18e9d73596d63f39e2f12ebc2c9d51e2eb06"),
-                ByteString.Parse("304402200c7fba6b623efd7e52731a11e6d7b99c2ae752c0f950b7a444ef7fb80162498c02202b01c74a4a04fb120860494de09bd6848f088927a7b07e3c3925b3894c8c89d4"));
+                signature);
 
-            Assert.False(signature.VerifySignature(payload.ToByteArray()));
+            Assert.False(evidence.VerifySignature(payload.ToByteArray()));
+
+            evidence = new SignatureEvidence(
+                ByteString.Parse("abcdef12345678"),
+                signature);
+
+            Assert.False(evidence.VerifySignature(payload.ToByteArray()));
         }
 
         [Fact]
         public void VerifySignature_InvalidSignature()
         {
-            SignatureEvidence signature = new SignatureEvidence(
+            SignatureEvidence evidence = new SignatureEvidence(
                 publicKey,
                 ByteString.Parse("304402200c7fba6b623efd7e52731a11e6d7b99c2ae752c0f950b7a444ef7fb80162498c02202b01c74a4a04fb120860494de09bd6848f088927a7b07e3c3925b3894c8c89d5"));
 
-            Assert.False(signature.VerifySignature(payload.ToByteArray()));
+            Assert.False(evidence.VerifySignature(payload.ToByteArray()));
 
-            signature = new SignatureEvidence(
+            evidence = new SignatureEvidence(
                 publicKey,
                 ByteString.Parse("3044"));
 
-            Assert.False(signature.VerifySignature(payload.ToByteArray()));
+            Assert.False(evidence.VerifySignature(payload.ToByteArray()));
 
-            signature = new SignatureEvidence(
+            evidence = new SignatureEvidence(
                 publicKey,
                 ByteString.Parse("300680044164616D"));
 
-            Assert.False(signature.VerifySignature(payload.ToByteArray()));
+            Assert.False(evidence.VerifySignature(payload.ToByteArray()));
         }
 
         [Fact]
         public void VerifySignature_InvalidLengths()
         {
-            SignatureEvidence signature = new SignatureEvidence(ByteString.Empty, ByteString.Empty);
+            SignatureEvidence evidence = new SignatureEvidence(ByteString.Empty, ByteString.Empty);
 
-            Assert.False(signature.VerifySignature(payload.ToByteArray()));
+            Assert.False(evidence.VerifySignature(payload.ToByteArray()));
         }
     }
 }
