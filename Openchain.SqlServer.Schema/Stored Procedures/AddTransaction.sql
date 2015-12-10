e36f966f0ed1be9f8b0fad143c3ee7f0e603f0ca
Flavien Charlon <flavien@charlon.net>
2015-12-10 14:58:25 +0000

    Create the AddTransaction stored procedure

---
+++ Openchain.SqlServer.Schema/Stored Procedures/AddTransaction.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [Openchain].[AddTransaction]
+    @instance int,
+    @transactionHash binary(32),
+    @mutationHash binary(32),
+    @rawData varbinary(MAX),
+    @records [Openchain].[RecordMutation] READONLY
+AS
+    SET XACT_ABORT ON
+    SET NOCOUNT ON
+
+    DECLARE @transactionId AS bigint
+
+    INSERT INTO [Openchain].[Transactions]
+    ([Instance], [TransactionHash], [MutationHash], [RawData])
+    OUTPUT Inserted.[Id] INTO @transactionId
+    VALUES (@instance, @transactionHash, @mutationHash, @rawData);
+
+    MERGE [Openchain].[Records] AS Target
+    USING @records AS Source
+    ON (Target.[Instance] = @instance) AND (Target.[Key] = Source.[Key]) AND (Target.[Version] = Source.[Version])
+    WHEN MATCHED THEN
+        UPDATE SET Target.[Value] = Source.[Value], Target.[Version] = @mutationHash
+    WHEN NOT MATCHED THEN
+        INSERT ([Instance], [Key], [Value], [Version], [Name], [Type])
+        VALUES (@instance, Source.[Key], Source.[Value], @mutationHash, Source.[Name], Source.[Type]);
+
+RETURN @transactionId
