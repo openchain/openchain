7ce2ad702802c41d2fe464e7f74e9abcb375e381
Flavien Charlon <flavien@charlon.net>
2015-12-10 14:28:09 +0000

    Add the RecordMutation type

---
+++ Openchain.SqlServer.Schema/Types/RecordMutation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TYPE [Openchain].[RecordMutation] AS TABLE
+(
+    [Key] VARBINARY(512) NOT NULL,
+    [Value] VARBINARY(MAX) NOT NULL,
+    [Version] VARBINARY(32) NOT NULL,
+    [Name] VARCHAR(512) NOT NULL,
+    [Type] TINYINT NOT NULL
+)
+GO
