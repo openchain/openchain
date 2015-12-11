521fc7168e690b21956494ab9ff6ef333638df82
Flavien Charlon <flavien@charlon.net>
2015-12-11 13:16:42 +0000

    Fix an issue with the RecordMutation type

--- Openchain.SqlServer.Schema/Types/RecordMutation.sql
+++ Openchain.SqlServer.Schema/Types/RecordMutation.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TYPE [Openchain].[RecordMutation] AS TABLE
 (
     [Key] VARBINARY(512) NOT NULL,
-    [Value] VARBINARY(MAX) NOT NULL,
+    [Value] VARBINARY(MAX) NULL,
     [Version] VARBINARY(32) NOT NULL,
     [Name] VARCHAR(512) NOT NULL,
     [Type] TINYINT NOT NULL
