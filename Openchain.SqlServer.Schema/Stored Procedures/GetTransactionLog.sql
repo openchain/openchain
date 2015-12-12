f3a3e3a96564b491cbb23ea7e3db1013c9d71698
Flavien Charlon <flavien@charlon.net>
2015-12-12 14:45:59 +0000

    Implement the GetTransactionLog stored procedure

---
+++ Openchain.SqlServer.Schema/Stored Procedures/GetTransactionLog.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [Openchain].[GetTransactionLog]
+    @instance INT,
+    @from VARBINARY(512),
+    @count INT
+AS
+    SET NOCOUNT ON;
+
+    IF @from IS NULL
+        SELECT TOP(@count) Transactions.[RawData]
+        FROM [Openchain].[Transactions]
+        WHERE Transactions.[Instance] = @instance
+        ORDER BY Transactions.[Id];
+    ELSE
+        SELECT TOP(@count) Transactions.[RawData]
+        FROM [Openchain].[Transactions]
+        WHERE Transactions.[Instance] = @instance
+            AND Transactions.[Id] >
+                (SELECT Transactions.[Id]
+                FROM [Openchain].[Transactions]
+                WHERE Transactions.[Instance] = @instance AND Transactions.[TransactionHash] = @from)
+        ORDER BY Transactions.[Id];
+
+RETURN
