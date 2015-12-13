97896440024cf4cf3c3a5001e66b85a5128cafce
Flavien Charlon <flavien@charlon.net>
2015-12-13 19:14:47 +0000

    Implement the GetTransaction stored procedure

---
+++ Openchain.SqlServer.Schema/Stored Procedures/GetTransaction.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [Openchain].[GetTransaction]
+    @instance INT,
+    @mutationHash BINARY(32)
+AS
+    SET NOCOUNT ON;
+
+    SELECT [RawData]
+    FROM [Openchain].[Transactions]
+    WHERE Transactions.[Instance] = @instance AND Transactions.[MutationHash] = @mutationHash;
+
+RETURN
