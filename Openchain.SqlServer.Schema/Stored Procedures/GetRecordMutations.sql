e70637fdd94dc1bba349f49163094aa4c88f4bec
Flavien Charlon <flavien@charlon.net>
2015-12-13 19:03:41 +0000

    Implement the GetRecordMutations stored procedure

---
+++ Openchain.SqlServer.Schema/Stored Procedures/GetRecordMutations.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [Openchain].[GetRecordMutations]
+    @instance INT,
+    @recordKey VARBINARY(256)
+AS
+    SET NOCOUNT ON;
+
+    SELECT Transactions.[MutationHash]
+    FROM [Openchain].[RecordMutations]
+    INNER JOIN [Openchain].[Transactions] ON RecordMutations.[TransactionId] = Transactions.[Id]
+    WHERE RecordMutations.[Instance] = @instance AND RecordMutations.[RecordKey] = @recordKey;
+
+RETURN
