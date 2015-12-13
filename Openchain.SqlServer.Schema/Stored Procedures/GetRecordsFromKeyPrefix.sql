85f462c88e747b4d4bf18cb44cf1c48a30f0bc63
Flavien Charlon <flavien@charlon.net>
2015-12-13 18:48:51 +0000

    Implement the GetRecordsFromKeyPrefix stored procedure

---
+++ Openchain.SqlServer.Schema/Stored Procedures/GetRecordsFromKeyPrefix.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [Openchain].[GetRecordsFromKeyPrefix]
+    @instance INT,
+    @prefix VARBINARY(512)
+AS
+    SET NOCOUNT ON;
+
+    SELECT [Key], [Value], [Version]
+    FROM [Openchain].[Records]
+    WHERE Records.[Instance] = @instance AND LEFT(Records.[Key], LEN(@prefix)) = @prefix;
+
+RETURN
