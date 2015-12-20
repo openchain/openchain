666af3597c0d0ae7972e69b0a489a881407cece3
Flavien Charlon <flavien@charlon.net>
2015-12-20 10:05:25 +0100

    Improve code documentation in the IComponentBuilder interface

--- src/Openchain.Infrastructure/IComponentBuilder.cs
+++ src/Openchain.Infrastructure/IComponentBuilder.cs
@@ -18,12 +18,30 @@ using Microsoft.Extensions.Configuration;
 
 namespace Openchain.Infrastructure
 {
+    /// <summary>
+    /// Represents a builder class capable of instanciating an object of a given type.
+    /// </summary>
+    /// <typeparam name="T">The type of object that can be instanciated by the builder.</typeparam>
     public interface IComponentBuilder<out T>
     {
+        /// <summary>
+        /// Gets the name of the builder.
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// Initializes the builder.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider for the current context.</param>
+        /// <param name="configuration">The builder configuration.</param>
+        /// <returns></returns>
         Task Initialize(IServiceProvider serviceProvider, IConfigurationSection configuration);
 
+        /// <summary>
+        /// Builds the target object.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider for the current context.</param>
+        /// <returns>The built object.</returns>
         T Build(IServiceProvider serviceProvider);
     }
 }
