14febe662f1d54b771506c9cf0f6e7ad73a8b1d5
Flavien Charlon <flavien@charlon.net>
2016-01-07 18:28:43 +0100

    Bump the version number to 0.5.0

--- src/Openchain.Server/Startup.cs
+++ src/Openchain.Server/Startup.cs
@@ -29,7 +29,7 @@ namespace Openchain.Server
 {
     public class Startup
     {
-        private static readonly string version = "0.5.0-rc1";
+        private static readonly string version = "0.5.0";
         private List<Task> runningTasks = new List<Task>();
         private readonly IConfiguration configuration;
 
